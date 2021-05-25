692fd514c86a9f6b177a6c2587a7fadfc71c4f73
anto991nio <antonio.91.cardone@gmail.com>
2021-05-25 09:57:08 +0200

    Termina footer

--- scripts/script.js
+++ scripts/script.js
@@ -25,16 +25,39 @@ const app = new Vue({
         {
             text: "LANDING",
             link: "#",
+        },],
+        findRestaurants:[{
+            indirizzo:"1614 E. Bell Rd #104.",
+            citta:"Salerno, AZ 85022",
+            numero:"(602) 867-1010",
+        },{
+            indirizzo:"204 E. Pizzetta Tommaso",
+            citta:"Sorrento, AZ 85022",
+            numero:"(358) 867-1010",
+        },{
+            indirizzo:"Vale puglia 54",
+            citta:"Torre Del Greco, AZ 85022",
+            numero:"(359) 867-1010",
+        },{
+            indirizzo:"Corso Italia AA",
+            citta:"Naples, AZ 85022",
+            numero:"(989) 867-1010",
+        },
+        ],
+        workDay:[{
+            day:"MONDAY",
+            closedKit:"Kitchen Closed",
+        },{
+            day:"TUESDAY UNTIL FRIDAY",
+            hours:"9:00 - 22:00",
+        },{
+            day:"SATURDAY ",
+            pos:"*",
+            hours:"Saturday 11am to midnight",
+        },{
+            day:"SUNDAY",
+            hours:"9:00 - 22:00",
         },]
     }, 
-   /*  activeMenu: "",
-    methods: {
-        onMenuClick(clickedMenu) {
-            this.activeMenu = clickedMenu;
-        }
-    },
-    
-    mounted() {
-        this.activeMenu = this.menuLeft[0]
-    } */
+   
 })
